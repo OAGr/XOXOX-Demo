4f47b61083026ffeea75b1b1e0c0d3a6e4652164
Ozzie Gooen <Ozzie_Gooen@hmc.edu>
2011-11-19 09:19:05 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xoxox::Application.config.secret_token = 'b79bfa4a0e0f05f2b3ef632aca400af03a9e68872584fe62a7558ba311f80b8f1869560d63d990af41111bf8b538cc66f1e4a05d84e4aeca218c9390e36ef5d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Xoxox::Application.config.session_store :cookie_store, key: '_xoxox_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Xoxox::Application.config.session_store :active_record_store
