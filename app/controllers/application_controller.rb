9a3fe2fc75eeb5cb028c8679fd0602a05d4d9903
Ozzie Gooen <Ozzie_Gooen@hmc.edu>
2011-11-19 21:53:19 -0800

    beta somewhat working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  def authenticate_user!
-    if !current_user
-      # This should work, but session is lost. See https://github.com/plataformatec/devise/issues/1357
-      # session[:return_to] = request.fullpath
-      redirect_to user_omniauth_authorize_path(:google_apps, :origin => request.fullpath)
-    end
-  end   
+
 
   def after_sign_in_path_for(resource)
     # This should work, but session is lost. See https://github.com/plataformatec/devise/issues/1357
