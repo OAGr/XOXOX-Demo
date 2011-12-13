0fdc7f8af86fd346c0db5bf98da59a96985c06e6
Ozzie Gooen <Ozzie_Gooen@hmc.edu>
2011-12-12 17:26:56 -0800

    added precompile to initilizer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,8 @@ Xoxox::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
 
+  config.assets.precompile += %w[active_admin.css active_admin.js]
+
   # Generate digests for assets URLs
   config.assets.digest = true
 
