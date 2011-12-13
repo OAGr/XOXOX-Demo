b352e8c1d14d171686c29b370329de0d7398c8df
Ozzie Gooen <Ozzie_Gooen@hmc.edu>
2011-12-12 17:04:32 -0800

    trying, removed most activeadmin stuff

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,18 +1,5 @@
 ActiveAdmin.register User do
   
-  index do
-    column "Name" do |user|
-         link_to user.name, admin_user_path(user)
-       end
-    column :bio
-    column :email
 
-    column "Offers" do |user| 
-      link_to(user.offers.count, admin_user_path(user)) 
-    end
-
-    default_actions
-  end
-  
 end
 
